5782ce9401640b5f7b96c22372923785c28c75cf
William Rodrigues <william.oliveira@infobrasil.local>
2024-09-26 13:47:03 -0300

    sla

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-let carros = [{"Marca": "Ferrari", "Modelo": "F40", "Ano": 1987}, {"Marca": "Ford", "Modelo": "GT500", "Ano": 1966}];
+let carros = [{"Marca": "Ferrari", "Modelo": "F40", "Ano": 1987}, {"Marca": "Ford", "Modelo": "GT500", "Ano": 1966}, {"Marca": "Nissan", "Modelo": "GTR SKYLINE R34", "Ano": 1998}];
 
 let msg = "";
+
+function descrever(carros){
+    return "Descrição: "  + "A marca deste carro é " + carros.Marca + ", além disso, o seu modelo é " + carros.Modelo + " e o seu ano de fabricação foi em " + carros.Ano; 
+}
 
 for(let i = 0; i < carros.length; i++){
     msg += "Marca: " + carros[i].Marca + "<br>";
     msg += "Modelo: " + carros[i].Modelo + "<br>";
-    msg += "Ano " + carros[i].Ano + "<br>";
+    msg += "Ano: " + carros[i].Ano + "<br>";
+    msg += descrever(carros[i]) + "<br>";
     msg += "<p>" + "</p>"
 }
 
-function descrever(){
-    for(let i = 0; i < carros.length; i++){
-        msg = "Esse carro é um(a) " + carros[i].Marca + "modelo " + carros[i].Modelo + "ano: " + carros[i].Ano;
-    }
-    return msg;
-}
-
 document.querySelector("#output").innerHTML = msg
-document.querySelector("#output").innerHTML += descrever()
